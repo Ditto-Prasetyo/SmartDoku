8480a024e74645a9970a892c37a2bf0b3c1d0e95
PenDZ-Alter <abxpendz45@gmail.com>
2025-07-26 20:42:55 +0700

    fix: overvalue from opacity

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -73,7 +73,7 @@ class LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
       ),
     );
 
-    _formAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+    _formAnimation = Tween<double>(begin: 0.0, end: 0.9).animate(
       CurvedAnimation(parent: _formController, curve: Curves.easeOutBack),
     );
 
