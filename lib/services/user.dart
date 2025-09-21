58c78321e296efb00a4e82e42a58ea1a2b80dc71
PenDZ-Alter <abxpendz45@gmail.com>
2025-09-21 15:39:46 +0700

    feat: adding service to list all filtered users

--- lib/services/user.dart
+++ lib/services/user.dart
@@ -1,7 +1,11 @@
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smart_doku/models/user.dart';
+import 'package:http/http.dart' as http;
+import 'package:smart_doku/utils/map.dart';
+import 'package:collection/collection.dart';
 
 class UserService {
   Future<UserModel?> getCurrentUser() async {
@@ -15,5 +19,37 @@ class UserService {
     }
 
     return null;
+  }
+
+  Future<Map<String, List<dynamic>>> getFilteredUsers(String bidang) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = await prefs.getString('jwt_token');
+    
+    if (token == null) throw Exception('Token tidak ditemukan');
+
+    final uri = Uri.parse("${dotenv.env['API_URL']}/user");
+
+    final response = await http.get(
+      uri,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${token}'
+      }
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+      
+      final List<dynamic> allData = data.map((e) => UserModel.fromJson(e)).toList();
+
+      final Map<String, List<dynamic>> groupedData = groupBy(
+        allData,
+        (user) => user.bidang,
+      );
+
+      return groupedData;
+    } else {
+      throw Exception('Gagal mengambil data users');
+    }
   }
 }
